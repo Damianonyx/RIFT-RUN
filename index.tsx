e4a4dc09f89026ccdea81561eb01e49c59139d07
GeekyDamian <abayomiekundayo9568@gmail.com>
2026-09-13 00:33:29 +0100

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { GameScreen } from "@/components/game/GameScreen";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <GameScreen />;
-}
